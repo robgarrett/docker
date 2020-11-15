0deb03b6e151cb37c71f44b780bfc2841df4d1a6
Rob Garrett <rob@robgarrett.com>
2020-11-14 21:20:09 -0500

    Working on service class

--- console/AlarmDotCom/src/app/index.ts
+++ console/AlarmDotCom/src/app/index.ts
@@ -1,8 +1,30 @@
-import Config from "./config";
+
+import AlarmDotComService from "./service";
+import { configure, getLogger } from "log4js";
 
 abstract class App {
+
     public static main() {
-        console.log("Hello World! " + Config.Instance.Username);
+        const logger = getLogger("AlarmDotCom");
+        logger.level = "debug";
+        configure({
+            appenders: { console: { type: "console" } },
+            categories: { default: { appenders: ["console"], level: "debug" } }
+        });
+        try {
+
+            logger.info("Alarm.com service started.");
+            const result = AlarmDotComService.login();
+            if (!result) { throw new Error("Login failure!") }
+        }
+        catch (err) {
+            if (err instanceof Error) {
+                logger.error((err as Error).message);
+            }
+        }
+        finally {
+            logger.info("Alarm.com service finished.")
+        }
     }
 }
 
